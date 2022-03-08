b15e8a4b8854c4932fafc7f9608b03a11768bef0
Davinder Singh <dsingh@Davinders-MacBook-Pro-2.local>
2022-03-08 16:23:41 +0000

    getting started with prjoect

---
+++ first-typescript-app/src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import FirstComponent from './components/FirstComponent'
+ReactDOM.render(
+    <div>
+      <h1>Hello, Welcome to React and TypeScript</h1>
+      <FirstComponent/>
+    </div>,
+    document.getElementById("root")
+);
